f2f5d73f4d662919d739c1937f59547f48d6bab6
Pezzini S <susanna.pezzini@gmail.com>
2019-05-08 13:03:35 +0700

    add local db ad env sample

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const body_parser = require('body-parser')
 const cors = require('cors')
-const session = require('express-session')
-const connect_mongodb = require('connect-mongodb-session')(session)
+// const session = require('express-session')
+// const connect_mongodb = require('connect-mongodb-session')(session)
 
 const app = express()
 require('dotenv').config()
@@ -15,31 +15,14 @@ app.use(body_parser.urlencoded({ // understands form submits
 }))
 app.use(cors())
 
-const session_store = new connect_mongodb({
-	uri: process.env.MONGODB_URL,
-	collection: 'sessions'
-})
 
-app.use(session({
-	secret: process.env.SESSION_SECRET,
-	unset: 'destroy',
-	resave: true,
-	rolling: true,
-	saveUninitialized: true,
-	store: session_store,
-	cookie: {
-		maxAge: 7 * 24 * 60 * 60 * 1000 // 1 week in milliseconds
-	}
-}))
 
-// Auth
-require('./auth.js')(app)
-// Routes
-require('./routes-client.js')(app)
+//routes
+
 require('./routes-api.js')(app)
-// Static
-app.use(express.static(__dirname + '/client')) // serves all files in the /client folder as static
-// Errors
+
+
+
 app.use(function(err, req, res, next) {
 	console.log('err', err);
 	res.status(400).json({
