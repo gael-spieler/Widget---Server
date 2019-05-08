eed81b7b40f624163bec8cafd2c385a918956286
KrisztaAnna <krisztina.baranyai@gmail.com>
2019-05-08 13:19:53 +0700

    declare tests for provider's bookings

---
+++ tests/03-provider-bookings.test.js
@@ -0,0 +1,37 @@
+import {describe} from "mocha";
+
+process.env.NODE_ENV = 'test'
+
+const chai = require('chai')
+const should = require('chai').should()
+const chaiHttp = require('chai-http')
+chai.use(chaiHttp)
+
+const server = require('../index')
+const db_provider = require('../models/provider')
+
+// Provider's bookings
+
+describe('provider bookings', function() {
+
+    it("should create a reserved time slot for provider", function(done) {
+
+    })
+
+    it("should provide the list of bookings a provider has", function(done) {
+
+    })
+
+    it("should update a booking to approved", function(done) {
+
+    })
+
+    it("should update a booking to cancelled", function(done) {
+
+    })
+
+    it("should update a booking to archived", function(done) {
+
+    })
+
+})
