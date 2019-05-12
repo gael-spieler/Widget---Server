f1f4a7d9bb9b6f85bd00cfdef543198f0b7f8c2c
KrisztaAnna <krisztina.baranyai@gmail.com>
2019-05-12 11:27:25 +0700

    delete stuff

--- controllers/customer_create.js
+++
@@ -1,8 +0,0 @@
-// Import db_customer
-const db_customer = require('../models/customer')
-
-module.exports = function(req, res, next) {
-	db_customer.create(req.body).then(function(new_customer) {
-		res.status(201).json(new_customer)
-	})
-}
