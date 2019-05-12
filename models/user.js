8e6e691a9c36d9aa1a2e8a40108c01262566048d
Pezzini S <susanna.pezzini@gmail.com>
2019-05-12 14:15:47 +0700

    add regExpression for email

--- models/user.js
+++ models/user.js
@@ -29,21 +29,22 @@ const user_schema = new schema({
 		required: true,
 		validate: [{
 			validator: function(email){
-				const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+				const expression = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/i;
 				return expression.test(String(email).toLowerCase())},
 			message: 'Invalid email address'
 		}]
 	},
 	password: {
 		type: String,
-		required: function(){
-			return this.google == null
-		},
+		// required: function(){
+		// 	return this.google == null
+		// },
+		required: true,
 		validate: [{
-			validator: function(email){
-				const expression = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/i;
-				return expression.test(String(email).toLowerCase())},
-			message: 'Invalid email address'
+			validator: function(password){
+				const expression = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/i;
+				return expression.test(String(password))},
+			message: 'Password must be different'
 		}]
 	},
 	status: {
@@ -62,12 +63,12 @@ const user_schema = new schema({
 	},
 	stripe_token: {
 		type: String,
-		required: true,
+		// required: true,
 		default: "abcd1234!@£$"
 	},
 	agreed_to_terms: {
 		type: Boolean,
-		required: true,
+		// required: true,
 		validate: [{
 			validator: function(agreement_to_terms){
 				return agreement_to_terms === true;
