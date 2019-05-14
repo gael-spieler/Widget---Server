8059c3d239eeaa8356f2ec68882baa06b07b3f92
KrisztaAnna <krisztina.baranyai@gmail.com>
2019-05-14 15:14:53 +0700

    fix archive service user id issue

--- controllers/service_archive.js
+++ controllers/service_archive.js
@@ -1,13 +1,22 @@
 // Import message
-const db_service = require('../models/service')
+const db_service = require('../models/service');
+
 
 module.exports = function(req, res, next) {
+
     db_service.findById(req.params.id, function(err, service){
+        console.log(service.provider);
+        console.log(req.headers.user_id);
+        if (service.provider != req.headers.user_id){
+            return next({
+                message: "Service doesn't exist."
+            })
+        }
         service.archived = req.body.archived;
         service.save(function(err, updated_service){
             res.status(200).json(updated_service)
         })
-    })
+    }).catch(next)
 };
 
 
