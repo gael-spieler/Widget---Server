06338f6364e2cde63302d01c070c211fb2d3cd3c
KrisztaAnna <krisztina.baranyai@gmail.com>
2019-05-16 08:46:05 +0700

    add populate to create booking controller

--- controllers/booking_create.js
+++ controllers/booking_create.js
@@ -1,26 +1,48 @@
 const db_booking = require('../models/booking');
 
 module.exports = function(req, res, next) {
-    db_booking.findOne({
-        service : req.body.service,
-        provider : req.body.provider,
-        start : req.body.start,
-        end : req.body.end,
-    }).then(function(booking) {
-        // make sure booking is unique
-        if (booking) {
-            return next({
-                message: 'Time slot is already booked.'
-            })
-        }
+    // db_booking.findOne({
+    //     service: req.body.service,
+    //     provider: req.body.provider,
+    //     start: req.body.start,
+    //     end: req.body.end,
+    // }).then(function (booking) {
+    //     // make sure booking is unique
+    //     if (booking) {
+    //         return next({
+    //             message: 'Time slot is already booked.'
+    //         })
+    //     }
+    //     // if booking does not exist, create the booking
+    //     let new_booking = req.body;
+    //     new_booking.customer = req.user;
+    //
+    //     db_booking.create(new_booking).then(function (new_booking) {
+    //         db_booking.findById(new_booking._id)
+    //             .populate({
+    //                 path: 'provider',
+    //                 select: 'first_name'
+    //             }).then(function (new_booking) {
+    //             res.status(201).json(new_booking)
+    //         })
+    //     }).catch(next())
+    // }).catch(next())
 
-        // if booking does not exist, create the booking
-        let new_booking = req.body;
+    let new_booking = req.body;
         new_booking.customer = req.user;
-
-        db_booking.create(new_booking).then(function(new_booking) {
-            res.status(200).json(new_booking)
-            // res.redirect('/')
-        }).catch(next)
-    }).catch(next)
+    db_booking.create(req.body).then(function(new_booking) {
+        db_booking
+            .findById(new_booking._id)
+            .populate({
+                path: 'provider',
+                select: ['first_name', 'last_name', 'company', 'currency']
+            })
+            .populate({
+                path: 'service',
+                select: ['name', 'length', 'price']
+            }).then(function(saved_booking) {
+            res.status(201).json(saved_booking)
+        })
+    })
 };
+
