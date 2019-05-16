f4c8ccf0276bae886909eae09d417c1af8212f95
KrisztaAnna <krisztina.baranyai@gmail.com>
2019-05-16 23:25:43 +0700

    add user_read controller

--- controllers/user_read.js
+++ controllers/user_read.js
@@ -1,8 +1,22 @@
 // Import db_user
-const db_user = require('../models/user')
+const db_user = require('../models/user');
+const db_booking = require('../models/booking');
 
 module.exports = function(req, res, next) {
-	db_user.findById(req.params.id).then(function(user) {
-		res.json(user)
+
+	db_user.findById(req.params.customer_id)
+		.then(function (customer) {
+
+		let result = {
+			name: customer.full_name,
+		};
+
+		db_booking.find({provider: req.user._id, customer: customer._id})
+			.then(function(bookings){
+				console.log('bookings', bookings[0].service);
+				result.bookings = bookings;
+				res.status(201).json(result)
+			})
 	})
-}
+};
+
