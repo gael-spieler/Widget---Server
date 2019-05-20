e614de83640e3c11ff21d3315fe63030a81fdb3e
KrisztaAnna <krisztina.baranyai@gmail.com>
2019-05-20 11:50:10 +0700

    routes

--- routes-client.js
+++ routes-client.js
@@ -3,7 +3,7 @@ module.exports = function(app){
 	// ### Client ###
 
 	app.get('/', require('connect-ensure-login').ensureLoggedIn(), function(req, res, next) {
-		res.sendFile(__dirname + '/client/bookings.html')
+		res.sendFile(__dirname + '/client/index.html')
 	});
 	app.get('/login', function(req, res, next) {
 		res.sendFile(__dirname + '/client/login.html')
