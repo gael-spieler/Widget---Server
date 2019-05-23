a034eb95fca17277550dea5c32e50fb2ead7487c
KrisztaAnna <krisztina.baranyai@gmail.com>
2019-05-23 15:49:04 +0700

    fix booking route and populate objects on read

--- controllers/bookings_read.js
+++ controllers/bookings_read.js
@@ -1,7 +1,16 @@
 const db_booking = require('../models/booking')
 
 module.exports = function(req, res, next) {
-    db_booking.find({provider: req.params.provider_id})
+    db_booking
+        .find({provider: req.user._id})
+        .populate({
+            path: 'customer',
+            select: ['first_name', 'last_name']
+        })
+        .populate({
+            path: 'service',
+            select: ['name', 'length', 'price']
+        })
         .then(function(bookings){
             res.status(200).json(bookings)
         })
