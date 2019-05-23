c98f1d3d65f2f987595ae4e2365439127e84e3d2
KrisztaAnna <krisztina.baranyai@gmail.com>
2019-05-23 23:22:17 +0700

    add order schema

---
+++ models/order.js
@@ -0,0 +1,44 @@
+// Import db.js
+const db = require('../db');
+
+// Import mongoose from node_modules
+const mongoose = require('mongoose');
+
+let schema = mongoose.Schema;
+let ObjectId = schema.Types.ObjectId;
+
+const options = {};
+
+options.timestamps =
+    {createdAt: 'created_at'},
+    {updatedAt: 'updated_at'};
+
+const order_schema = new schema({
+    __v: {
+        type: Number,
+        select: false
+    },
+    provider: {
+        type: ObjectId,
+        required: true
+    },
+    customer: {
+        type: ObjectId,
+        required: true
+    },
+    product: {
+        type: string,
+        required: true
+    },
+    stripe_id: {
+        type: string,
+        required: true
+    }
+    },
+    options);
+
+
+const db_order= new db.model('order', order_schema);
+
+// Export
+module.exports = db_order;
