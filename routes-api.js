645e9e517cee7e3160c4dd9b8549196f3b7807ce
KrisztaAnna <krisztina.baranyai@gmail.com>
2019-05-24 14:01:02 +0700

    route

--- routes-api.js
+++ routes-api.js
@@ -56,4 +56,8 @@ module.exports = function(app){
 
 	// Orders
 	app.post('api/orders', require('./controllers/order_create'));
+
+	app.get('/', function(req, res, next){
+		res.send('<h1>Hello</h1>')
+	})
 };
