093628590902b3ec9ee66a23fdcff35274ff351e
KrisztaAnna <krisztina.baranyai@gmail.com>
2019-05-24 12:21:24 +0700

    send email to provider about new booking

--- controllers/booking_create.js
+++ controllers/booking_create.js
@@ -23,7 +23,7 @@ module.exports = function(req, res, next) {
             .findById(new_booking._id)
             .populate({
                 path: 'provider',
-                select: ['first_name', 'last_name', 'company', 'currency', 'cancellation_time']
+                select: ['first_name', 'last_name', 'company', 'currency', 'cancellation_time', 'email']
             })
             .populate({
                 path: 'service',
@@ -32,16 +32,27 @@ module.exports = function(req, res, next) {
 
                 db_user.findById(req.body.customer).then(function(user) {
                     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-                    const msg = {
+                    const msg_to_customer = {
                         to: user.email,
                         from: process.env.EMAIL_ADDRESS,
                         subject: 'You booked an appointment with ' + saved_booking.provider.company + '!',
                         // text: 'and easy to do anywhere, even with Node.js',
                         html: '<strong>Dear ' + user.first_name + ', thank you for booking with Platboo!  You booked ' + saved_booking.service.name + ' with '+ saved_booking.provider.company +' on ' + saved_booking.start + '.  Happy booking!</strong>',
                     };
-                    sgMail.send(msg);
+
+                    const msg_to_provider = {
+                        to: saved_booking.provider.email,
+                        from: process.env.EMAIL_ADDRESS,
+                        subject: "You have a new booking!",
+                        // text: 'and easy to do anywhere, even with Node.js',
+                        html: '<strong>Dear ' + saved_booking.provider.first_name + ', ' + user.first_name + ' ' +  user.last_name +' has just booked an appointment with you! Booking details: ' + saved_booking.service.name + ' on ' + saved_booking.start + '.  Best regards, Platboo Team!</strong>',
+                    };
+                    
+                    sgMail.send(msg_to_customer);
+                    sgMail.send(msg_to_provider);
                     res.status(201).json(saved_booking)
                 });
+
         })
     })
 })};
